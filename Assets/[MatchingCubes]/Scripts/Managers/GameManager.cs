f931ba2650eebd655fc5d7b42398ca84246c7fb7
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2022-09-30 02:30:34 +0300

    Update GameManager.cs

--- Assets/[MatchingCubes]/Scripts/Managers/GameManager.cs
+++ Assets/[MatchingCubes]/Scripts/Managers/GameManager.cs
@@ -3,31 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
-{    
-    public float Coin { get; set; }
+{
+    private float _coin;
+    public float Coin 
+    {
+        get 
+        {
+            return _coin;
+        }
+
+        set 
+        {
+            _coin = value;
+            PlayerPrefs.SetFloat(PlayerPrefsKeys.Coin, _coin);
+        }
+    }
 
     private void Awake()
     {
         LoadData();
-    }
-
-    private void SaveData() 
-    {
-        PlayerPrefs.SetFloat(PlayerPrefsKeys.Coin, Coin);
-    }
+    }  
 
     private void LoadData() 
     {
         Coin = PlayerPrefs.GetFloat(PlayerPrefsKeys.Coin, 0);
-    }
-
-    private void OnApplicationPause(bool pause)
-    {
-        SaveData();
-    }
-
-    private void OnApplicationQuit()
-    {
-        SaveData();
-    }
+    }    
 }
